4a7f6cc804eedc254be0d42d41f5064ce4ce466b
RomainAsus <romain.marcazzan@hotmail.com>
2025-02-08 05:03:13 +0100

    feat: add CORS support to the application and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
