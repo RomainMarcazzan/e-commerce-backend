db45b7441d0c74f4beae747a4d91f753cccb42a2
RomainAsus <romain.marcazzan@hotmail.com>
2025-02-19 19:02:13 +0100

    feat: update API documentation and dependencies for improved schema handling

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,13 +1,11 @@
 import swaggerUi from "swagger-ui-express";
-import fs from "fs";
-import yaml from "js-yaml";
 import { Express } from "express";
+import RefParser from "@apidevtools/json-schema-ref-parser";
 
 const pathToYaml = __dirname + "/swagger.yaml";
-const swaggerDocument = yaml.load(
-  fs.readFileSync(pathToYaml, "utf8")
-) as Record<string, any>;
 
-export const setupSwagger = (app: Express) => {
+// Asynchronously load and dereference swagger.yaml before setting up swagger middleware
+export const setupSwagger = async (app: Express) => {
+  const swaggerDocument = await RefParser.dereference(pathToYaml);
   app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
