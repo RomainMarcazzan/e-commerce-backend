ef5e28baa1de97d5f50e68a536114b37ed8feb7f
RomainAsus <romain.marcazzan@hotmail.com>
2025-02-19 14:05:53 +0100

    feat: remove swagger-jsdoc integration and switch to YAML for API documentation

--- src/config/generate-swagger.ts
+++
@@ -1,9 +0,0 @@
-// generate-swagger.js
-import swaggerJsdoc from "swagger-jsdoc";
-import { options } from "./swagger";
-import fs from "fs";
-
-const swaggerSpec = swaggerJsdoc(options);
-
-// Write swagger.json
-fs.writeFileSync("./swagger.json", JSON.stringify(swaggerSpec, null, 2));
--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,26 +1,13 @@
-import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
 import { Express } from "express";
 
-export const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Ecommerce API",
-      version: "1.0.0",
-      description: "API documentation for the Ecommerce application",
-    },
-    servers: [
-      {
-        url: "http://localhost:5000/v1",
-      },
-    ],
-  },
-  apis: ["./src/routes/*.ts", "./src/controllers/*.ts"], // Path to the API docs
-};
-
-const swaggerSpec = swaggerJsdoc(options);
+const pathToYaml = __dirname + "/swagger.yaml";
+const swaggerDocument = yaml.load(
+  fs.readFileSync(pathToYaml, "utf8")
+) as Record<string, any>;
 
 export const setupSwagger = (app: Express) => {
-  app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
